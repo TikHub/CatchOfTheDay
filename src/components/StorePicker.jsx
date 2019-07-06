443b65d457cba7d07aa82686a9fb4b2533ec4c39
Tigran <soghomonyantigran@gmail.com>
2019-07-07 00:27:07 +0400

    13: AddFishForm

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 export default class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = event => {
+    event.preventDefault();
+    // console.log(this.myInput.current.value);
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
